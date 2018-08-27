529da542d35e0a4e2b6df9ffdf2b3fecf758b0fa
Zach Wernberg <zawerny@gmail.com>
2018-08-27 09:50:14 -0500

    fix cathy owner

--- owners/serializers.py
+++ owners/serializers.py
@@ -4,12 +4,13 @@ from rest_framework import serializers
 class OwnerSerializer(serializers.Serializer):
     teamId = serializers.IntegerField()
     owner = serializers.SerializerMethodField()
-
-    # def get_teamName(self, obj):
-    #     return (obj['teamLocation'] + " " + obj['teamNickname'])
-
     def get_owner(self, obj):
         owner = obj['owners'][0]
         if (obj['teamAbbrev'] == 'PRIN'):
             owner = obj['owners'][1]
+        elif (obj['teamAbbrev'] == 'cw'):
+            owner = {
+                'firstName': 'Cathy',
+                'lastName': 'Wernberg'
+            }
         return (owner['firstName'] + ' ' + owner['lastName'])
